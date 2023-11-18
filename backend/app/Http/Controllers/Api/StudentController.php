96318a5a0bfec238ac817393d110351650e57456
Auriel James <aurieljames11@gmail.com>
2023-11-19 01:48:05 +0800

    issue/#8 Post Method Done

--- backend/app/Http/Controllers/Api/StudentController.php
+++ backend/app/Http/Controllers/Api/StudentController.php
@@ -9,8 +9,15 @@ use Illuminate\Http\Request;
 class StudentController extends Controller
 {
     //
-    public function createStudent(Request $request){
+    public function createStudent(Request $request)
+    {
         Student::create($request->all());
         return response()->json("Student has been saved to database");
+    }
+
+    public function getStudents()
+    {
+        $students = Student::all();
+        return response()->json($students);
     }
 }
