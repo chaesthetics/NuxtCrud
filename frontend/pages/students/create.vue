99f8af768b54e75326b19b741fe3c77e89df4680
Auriel James <aurieljames11@gmail.com>
2023-11-23 00:20:02 +0800

    issue/#13 Validator Response Implemented as Reusable Component

--- frontend/pages/students/create.vue
+++ frontend/pages/students/create.vue
@@ -11,13 +11,28 @@ const initialState = {
     phone: '',
 };
 
+const validationError = {
+    name: false,
+    course: false,
+    email: false,
+    phone: false,
+}
+
 const student = reactive({...initialState});
 
 const isLoading = ref(false);
 const isLoadingTitle = ref('Loading');
 
 const saveStudent = async() => {
+    console.log(errors.value?.data?.name[0]);
+    isLoading.value = true;
+    isLoadingTitle.value = 'Saving';
+    console.log(isLoadingTitle.value);
     await storeStudents(student);
+    isLoading.value = false;
+    isLoadingTitle.value = 'Loading';
+
+    validationError.value = errors.value.data;
 }
 
 </script>
@@ -38,18 +53,30 @@ const saveStudent = async() => {
                     <div class="mb-3">
                         <label for="name"></label>
                         <input v-model="student.name" id="name" type="text" class="form-control"/>
+                        <div v-if="validationError.value?.name">
+                            <Validator :message="validationError.value.name[0]"/>
+                        </div> 
                     </div>
                     <div class="mb-3">
                         <label for="course">Course</label>
                         <input v-model="student.course" id="course" type="text" class="form-control"/>
+                        <div v-if="validationError.value?.course">
+                            <Validator :message="validationError.value.course[0]"/>
+                        </div> 
                     </div>
                     <div class="mb-3">
                         <label for="email">Email</label>
                         <input v-model="student.email" id="email" type="text" class="form-control"/>
+                        <div v-if="validationError.value?.email">
+                            <Validator :message="validationError.value.email[0]"/>
+                        </div> 
                     </div>
                     <div class="mb-3">
                         <label for="phone">Phone</label>
                         <input v-model="student.phone" id="phone" type="text" class="form-control"/>
+                        <div v-if="validationError.value?.phone">
+                            <Validator :message="validationError.value.phone[0]"/>
+                        </div> 
                     </div>
                     <div class="mb-3">
                         <button class="btn btn-primary" type="submit">Create Student</button>
