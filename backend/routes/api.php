9547ad513e6d54b867a8678db077c08e0c1d68cf
Auriel James <aurieljames11@gmail.com>
2023-11-25 07:58:53 +0800

    issue/#24 Delete Method Done

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/createStudents', [StudentController::class, 'createStudent']);
 Route::get('/getStudents', [StudentController::class, 'getStudents']);
+Route::delete('/deleteStudent/{id}', [StudentController::class, 'deleteStudent']);
